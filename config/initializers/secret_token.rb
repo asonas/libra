7e81d5aebbf84325b7b2e977763d6ffd544e118a
asonas <hzw1258@gmail.com>
2013-05-09 14:58:13 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Libra::Application.config.secret_token = '0ce642f28312f0e5753bc39e9cfcfbb363b5ad261696b72ce8e047ff5808e551600d8e75b6c576c2f39df8c22d81dd4b88c77ef5e6bb68d34b745a0e54210fce'
